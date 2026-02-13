bd6f46124ef50c6be7af40701ae37a3863427526
AlonzoBustamante <ispy600@gmail.com>
2026-02-13 10:47:55 +0800

    changed font style to Bobby Jones and changed font color of hero section text.Also centered the text.

--- src/lib/components/herosection/hero.svelte
+++ src/lib/components/herosection/hero.svelte
@@ -1,11 +1,11 @@
 <div class=" bg-white text-center ">
-    <div>
+    <div class = "text-5xl text-[#FF914B]" style="font-family: 'Bobby Jones Soft', sans-serif; ">
         ALONZO BUSTAMANTE
     </div>
-    <div>
+    <div  class = "text-2xl text-[#FF914B]" style="font-family: 'Bobby Jones Soft', sans-serif;">
         WELCOME TO MY COFFEE SHOP
     </div>
-    <div>
+    <div  class = "text-2xl text-[#FF914B]" style="font-family: 'Bobby Jones Soft', sans-serif;">
         I WRITE CODE, BUILD APPS AND LOVE TO DRINK COFFEE.
     </div>
         
