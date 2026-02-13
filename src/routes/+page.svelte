60b50015a16024c0d2afc80cbae0d400fb761ea0
AlonzoBustamante <ispy600@gmail.com>
2026-02-13 09:37:39 +0800

    navigation bar: Added responsive navigation bar.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Header from '../lib/components/navigation/header.svelte'
+</script>
+
+<Header/>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: #FF914B;
+  }
+</style>
