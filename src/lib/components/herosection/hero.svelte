322a7cf856fc450ce8c682e7c64d1ba0477227f5
AlonzoBustamante <ispy600@gmail.com>
2026-02-13 11:03:40 +0800

    added svg image of me drinking coffee for hero section. Added image to assets folder. Added svg image to hero.svelte.

--- src/lib/components/herosection/hero.svelte
+++ src/lib/components/herosection/hero.svelte
@@ -1,3 +1,7 @@
+<script>
+   import HeroImage from "../../assets/DrinkingCoffeeCharacter.svg"
+</script>
+
 <div class=" bg-white text-center ">
     <div class = "text-5xl text-[#FF914B]" style="font-family: 'Bobby Jones Soft', sans-serif; ">
         ALONZO BUSTAMANTE
@@ -9,4 +13,8 @@
         I WRITE CODE, BUILD APPS AND LOVE TO DRINK COFFEE.
     </div>
         
-    </div>
+</div>
+<div>
+   <img src={HeroImage} alt="A cartoon of me drinking coffee with a cat beside me."/>
+
+</div>
