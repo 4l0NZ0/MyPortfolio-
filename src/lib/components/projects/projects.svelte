bf7e04cb142fa15c9abd506efcfddbe4fe737321
AlonzoBustamante <ispy600@gmail.com>
2026-02-19 16:08:05 +0800

    Updated project section. Added list of projects I have done.Put them into a list with a description.

--- src/lib/components/projects/projects.svelte
+++ src/lib/components/projects/projects.svelte
@@ -3,7 +3,25 @@
 
 </script>
 
-<div class=" flex flex-col items-center bg-[#EFE4D6] text-center ">
+<div class=" flex flex-col items-center bg-[#EFE4D6]  ">
     <SectionHeader sectionTitle="Projects" />
+    <div>
+            <dl class="flex flex-col items-center">
+                <dt style="font-family: 'Bobby Jones Soft', sans-serif; ">
+                    <a data-sveltekit-preload-data="hover" href="/stonks">
+	NIHAO PINYIN ...................[Learn More]
+</a></dt>
+                <dd>Swift app that adds pinyin to chinese characters.</dd>
+                <br>
+                
+                <dt style="font-family: 'Bobby Jones Soft', sans-serif; ">NETFLIX CLONE ...............[Learn More]</dt>
+                <dd>Swift app that adds pinyin to chinese characters.</dd>
+                <br>
+                
+                <dt style="font-family: 'Bobby Jones Soft', sans-serif; ">SUWON BITES...................[Learn More]</dt>
+                <dd>Swift app that adds pinyin to chinese characters.</dd>
+                <br>
+            </dl>
+    </div>
 
     </div>
