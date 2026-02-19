03967f60fb7d8b397e52ef2f08a9fdcae17e78ee
AlonzoBustamante <ispy600@gmail.com>
2026-02-19 17:49:15 +0800

    Added logo images to assets folder. Added new component in project section. Includes image of project/Logo, project name, and description. Edited sectionheaders file. Changed style of header sections to not include a background. Make it look less like a button.

--- src/lib/components/projects/projects.svelte
+++ src/lib/components/projects/projects.svelte
@@ -1,13 +1,49 @@
 <script lang="ts">
     import SectionHeader from "../sectionheaders/sectionheaders.svelte"
 
+   import NiHaoPinyinLogo from "../../assets/NihaoPinyinLogo.svg"
+   import SuwonLogo from "../../assets/SuwonLogo.svg"
+
+
 </script>
 
 <div class=" flex flex-col items-center bg-[#EFE4D6]  ">
     <SectionHeader sectionTitle="Projects" />
     <div>
+
+        <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+  <img class="mx-auto block h-20 rounded-full sm:mx-0 sm:shrink-0" src={NiHaoPinyinLogo} alt="" />
+  <div class="space-y-2 text-center sm:text-left">
+    <div class="space-y-0.5">
+      <p class="text-lg font-semibold text-black" style=" font-family: 'Bobby Jones Soft', sans-serif; ">NIHAO PINYIN</p>
+      <p class="font-medium text-gray-500">Swift app that adds pinyin to chinese characters.</p>
+    </div>
+    <button class="border-green-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ..." style=" font-family: 'Bobby Jones Soft', sans-serif; ">
+      Lean More
+    </button>
+  </div>
+</div>
+
+
+
+    <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+  <img class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src={SuwonLogo} alt="" />
+  <div class="space-y-2 text-center sm:text-left">
+    <div class="space-y-0.5">
+      <p class="text-lg font-semibold text-black" style=" font-family: 'Bobby Jones Soft', sans-serif; ">Suwon Bites</p>
+      <p class="font-medium text-gray-500">Swift app that adds pinyin to chinese characters.</p>
+    </div>
+    <button class=" bg-[#FF914B] rounded-full p-2 text-[#EFE4D6]" style="font-family: 'Bobby Jones Soft', sans-serif;">
+      Lean More
+    </button>
+  </div>
+</div>
+
+
+
+
             <dl class="flex flex-col items-center">
-                <dt style="font-family: 'Bobby Jones Soft', sans-serif; ">
+                <dt style=" font-family: 'Bobby Jones Soft', sans-serif; bg ">
                     <a data-sveltekit-preload-data="hover" href="/stonks">
 	NIHAO PINYIN ...................[Learn More]
 </a></dt>
--- src/lib/components/sectionheaders/sectionheaders.svelte
+++ src/lib/components/sectionheaders/sectionheaders.svelte
@@ -3,6 +3,6 @@
  </script>
  
  
- <div class = " bg-[#FF914B]  rounded-full w-fit border-4 p-3 text-2xl text-[#EFE4D6]" style="font-family: 'Bobby Jones Soft', sans-serif;">
+ <div class = " text-2xl text-[#FF914B]" style="font-family: 'Bobby Jones Soft', sans-serif;">
     {props.sectionTitle}
     </div>
