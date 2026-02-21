45b3645812a67131d78a966816897d95eb46a245
AlonzoBustamante <ispy600@gmail.com>
2026-02-21 10:03:00 +0800

    Added card component to project section. Added new folder called project card component. This folder will hold the svelte file which will contain the project section card component. This component will have information about the each project. Their name, description and a Learn More button.

--- src/lib/components/projects/projects.svelte
+++ src/lib/components/projects/projects.svelte
@@ -11,22 +11,22 @@
     <SectionHeader sectionTitle="Projects" />
     <div>
 
-        <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+     <div class="   rounded-xl flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
   <img class="mx-auto block h-20 rounded-full sm:mx-0 sm:shrink-0" src={NiHaoPinyinLogo} alt="" />
   <div class="space-y-2 text-center sm:text-left">
     <div class="space-y-0.5">
-      <p class="text-lg font-semibold text-black" style=" font-family: 'Bobby Jones Soft', sans-serif; ">NIHAO PINYIN</p>
+      <p class="text-lg font-semibold text-black" style=" font-family: 'Bobby Jones Soft', sans-serif; ">NIHAO PINYIN </p>
       <p class="font-medium text-gray-500">Swift app that adds pinyin to chinese characters.</p>
     </div>
-    <button class="border-green-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ..." style=" font-family: 'Bobby Jones Soft', sans-serif; ">
-      Lean More
+    <button class="border p-2 text-[#FF914B] hover:border-transparent hover:bg-[#FF914B] hover:text-white active:bg-[#FF914B] ..." style=" font-family: 'Bobby Jones Soft', sans-serif; ">
+      Learn More
     </button>
   </div>
 </div>
 
 
 
-    <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+    <div class="  rounded-xl flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
   <img class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src={SuwonLogo} alt="" />
   <div class="space-y-2 text-center sm:text-left">
     <div class="space-y-0.5">
