a63271f9b02706a96b0dfd3a9fcc662d707c0960
AlonzoBustamante <ispy600@gmail.com>
2026-03-09 19:19:38 +0800

    Made our schema for our form using zod. Added a form in our page.svelte. The user is able to type input in and using the SuperDebug can see the input from the user.The form is currently not able to submit.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,21 @@
-import { superValidate } from "sveltekit-superforms";
+import { superValidate, message } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import { contactSchema } from "$lib/schemas/contact";
-
 import { fail } from "@sveltejs/kit";
 
 export const load = async () => {
   const form = await superValidate(zod(contactSchema));
-
-  // Always return { form } in load functions
   return { form };
+};
+
+export const actions = {
+  default: async ({ request }) => {
+    const form = await superValidate(request, zod(contactSchema));
+
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    return message(form, "Form posted succesfully");
+  },
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
-<script>
-    import Header from '../lib/components/navigation/header.svelte'
-    import Hero from '../lib/components/herosection/hero.svelte'
-    import AboutMe from '../lib/components/aboutme/aboutme.svelte'
-    import Projects from '../lib/components/projectssection/projects.svelte'
-    import Skills from '../lib/components/skillssection/skillssection.svelte'
+<script lang="ts">
+    import Header from '../lib/components/navigation/header.svelte';
+    import Hero from '../lib/components/herosection/hero.svelte';
+    import AboutMe from '../lib/components/aboutme/aboutme.svelte';
+    import Projects from '../lib/components/projectssection/projects.svelte';
+    import Skills from '../lib/components/skillssection/skillssection.svelte';
     import Contact from '$lib/components/contact/contact.svelte';
+    import {superForm} from 'sveltekit-superforms';
+    import SuperDebug from 'sveltekit-superforms';
+
+    const { data } = $props();
+    const { form } = superForm(data.form);
+
+
+
 </script>
+
 
 <div class="container mx-auto px-3">
 <Header/>
@@ -14,9 +23,34 @@
 <Projects/>
 <Skills/>
 <Contact/>
-  
+
+<form method="POST">
+<div class ="grid gap-3 grid-cols-1 text-[#191A14] " >
+
+<div class ="grid grid-cols-1">
+ <label style="font-family: 'The Retro Font', sans-serif; " for="name">NAME</label>
+  <input  class = "border-6 border-[#191A14] " type="text" name="name" bind:value={$form.name}>
 </div>
 
+<div class ="grid grid-cols-1">
+   <label style="font-family: 'The Retro Font', sans-serif; " for="email">E-mail</label>
+  <input class = "border-6 border-[#191A14]" type="email" name="email" bind:value={$form.email} />
+</div>
+
+<div class ="grid grid-cols-1">
+  <label style="font-family: 'The Retro Font', sans-serif; " for="message">Message</label>
+  <textarea class = "border-6 border-[#191A14]" rows="4" name="message" bind:value={$form.message} ></textarea>
+</div>
+  <div class="grid grid-cols-1  place-content-center  "> 
+<button style="font-family: 'The Retro Font', sans-serif; " class=" text-4xl border-6 p-1 text-[#F14133] hover:border-transparent hover:bg-[#F14133] hover:text-[#EFE4D6] active:bg-[#F81200]" >
+        Submit
+    </button></div>
+</div>
+ 
+</form>
+  
+</div>
+<SuperDebug data = {$form}/>
 
 
 <style lang="postcss">
