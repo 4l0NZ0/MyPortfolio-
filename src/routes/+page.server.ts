bbdc59c5f8c491863c9af63f7f7f666fe3b0f3fc
AlonzoBustamante <ispy600@gmail.com>
2026-03-09 13:11:58 +0800

    Fixed error. Need zod v3 in orwith super forms to fix type error.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,11 @@
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import { contactSchema } from "$lib/schemas/contact";
-import { z } from "zod/v3";
 
 import { fail } from "@sveltejs/kit";
 
-// Define outside the load function so the adapter can be cached
-const schema = z.object({
-  name: z.string().default("Hello world!"),
-  email: z.string().email(),
-});
-
 export const load = async () => {
-  const form = await superValidate(zod(schema));
+  const form = await superValidate(zod(contactSchema));
 
   // Always return { form } in load functions
   return { form };
